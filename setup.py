0317897f4241cc34bc62b24def803291d7b36b3c
GUILHEM_FAURE <guilhemfaure@outlook.fr>
2023-10-25 20:48:37 +0200

    Modified setup.py

--- setup.py
+++ setup.py
@@ -6,15 +6,15 @@ root_path = os.curdir
 with open(f"{root_path}/requirements.txt") as f:
     requirements = f.read().splitlines()
 
-VERSION = "0.0.1"
+VERSION = "0.0.15"
 DESCRIPTION = "Warning: this package is in alpha stage of development. Use at your own risk."
 # LONG_DESCRIPTION = ""
 
 setup(
     name="raop",
     version=VERSION,
-    author="Guilhem Fauré",
-    author_email="<guilhemfaure@outlook.fr>",
+    author="GFaure9",
+    author_email="",
     description=DESCRIPTION,
     # long_description=LONG_DESCRIPTION,
     packages=find_packages(),
@@ -24,6 +24,6 @@ setup(
         "Intended Audience :: Financial and Insurance Industry",
         "Programming Language :: Python :: 3.11",
         "Operating System :: OS Independent",
-    ]
-    # url='https://github.com/yourusername/package_name',
+    ],
+    url="https://github.com/GFaure9/raop",
 )
